db1e8e8f7250d85c2eaddda3e259ab28d270f170
Gabriel Viana <aqua.camus@hotmail.com>
2020-09-24 17:45:06 -0300

    ComponentDidAmount and DidUpdate

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 
 class ClassComponent extends Component<{name: string}> {
 
+    constructor(props: any){
+        super(props);
+        console.log("constructor  reached");
+        
+    }
+
     state = {
         name: "Mundo!!"
     }
 
+    componentDidMount(){
+        console.log("didMount  reached");
+        
+    }
+
+    componentDidUpdate(){
+        console.log("diduPDATE  reached");
+        
+    }
+
     render() {
+        console.log("render reached");
+        
         return (
             <div>
                 <p>Name: {this.state.name} </p> 
