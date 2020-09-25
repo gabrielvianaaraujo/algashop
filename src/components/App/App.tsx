feccd779e165f4d6083f117c88e4cecc953bbaaf
Gabriel Viana <aqua.camus@hotmail.com>
2020-09-25 17:22:19 -0300

    Clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent';
-// import ClassComponent from '../ClassComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <TestComponent name = "Gabriel"/>
-          {/* <ClassComponent name = "Senhor Dev"/> */}
-        </p>
-      </header>
+      App
     </div>
   );
 }
