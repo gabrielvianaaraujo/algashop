0b03c799c962bf0acb77f7347477bc955b00beb7
Gabriel Viana <aqua.camus@hotmail.com>
2020-09-27 01:33:43 -0300

    add container and move button to shared directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
 
+function TestComponent (){
+  return <img width = "16px" src="https://png.pngtree.com/png-vector/20190115/ourmid/pngtree-vector-search-icon-png-image_320926.jpg" alt="" />
+}
 
 function App() {
   return (
     <div className="App">
       <Header title = "AlgaStock" />
-      App
+
+      <Container>
+        <Button onClick={()=> window.alert('Olá')}
+          appendIcon={<TestComponent />}>
+          
+        </Button>
+      </Container>
+
     </div>
   );
 }
