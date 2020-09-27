72192ad587ae78cb23aff2cf722f2f50bc1dd7a7
Gabriel Viana <aqua.camus@hotmail.com>
2020-09-27 01:54:35 -0300

    add input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input';
 
 function TestComponent (){
   return <img width = "16px" src="https://png.pngtree.com/png-vector/20190115/ourmid/pngtree-vector-search-icon-png-image_320926.jpg" alt="" />
 }
 
 function App() {
+
+  const [street, setStreet] = useState('');
+
   return (
     <div className="App">
       <Header title = "AlgaStock" />
@@ -18,6 +22,14 @@ function App() {
           appendIcon={<TestComponent />}>
           
         </Button>
+
+        <Input
+          label="Street"
+          placeholder="E.g.: 15h Avenue"
+          value={street}
+          onChange={e => setStreet(e.target.value)}
+        />
+        
       </Container>
 
     </div>
