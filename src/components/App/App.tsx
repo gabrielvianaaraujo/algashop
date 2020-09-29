3d46d97cbf6c3156c0e9616057ec5dd5667b0376
Gabriel Viana <aqua.camus@hotmail.com>
2020-09-29 01:58:13 -0300

    Button component, props and more

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../Button';
+
+function TestComponent() {
+  return <img src="https://png.pngtree.com/png-vector/20190115/ourmid/pngtree-vector-search-icon-png-image_320926.jpg" alt="search"/>
+}
 
 
 function App() {
   return (
     <div className="App">
       <Header title = "AlgaStock" />
-      App
+
+      <div className="Container">
+        <Button 
+        onClick={() => window.alert('Clicado')}
+        appendIcon={<TestComponent />}>
+          Alert
+        </Button>
+      </div>
+
     </div>
   );
 }
